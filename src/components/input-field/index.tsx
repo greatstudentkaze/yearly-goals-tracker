c99fa3403ade4f68248b5d62b0f8f18a6ca2c658
greatstudentkaze <dfyz0r@list.ru>
2022-03-02 18:58:04 +0300

    feat(components): add InputField

---
+++ src/components/input-field/index.tsx
@@ -0,0 +1,20 @@
+import cn from 'classnames';
+
+import { InputFieldProps } from './index.props';
+
+const InputField = ({
+    labelText,
+    id,
+    type = 'text',
+    className,
+    ...props
+}: InputFieldProps) => {
+    return (
+        <div className={cn('input-field', className)}>
+            <label className="input-field__label" htmlFor={id}>{labelText}</label>
+            <input className="input-field__input" type={type} id={id} {...props} />
+        </div>
+    );
+};
+
+export default InputField;
