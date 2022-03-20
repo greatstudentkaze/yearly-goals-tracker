d1867b68206e9daf574ff3cc2f78e671a0c07051
greatstudentkaze <dfyz0r@list.ru>
2022-03-20 09:59:57 +0300

    feat: add routing and app context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-import Header from './components/header';
+import { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router';
 
-function App() {
-  return (
-    <Header />
-  );
-}
+import { AppContext } from './app.context';
+import { IGoal } from './interfaces/interfaces';
+import { AppRoute } from './contants';
+
+import Login from './screens/login';
+import Home from './screens/home';
+import CreateGoal from './screens/create-goal';
+import GoalDetailsScreen from './screens/goal-details';
+import Feedback from './screens/feedback';
+
+const App = () => {
+    const [isAuthorized, setIsAuthorized] = useState(false);
+    const [goals, setGoals] = useState<IGoal[] | null>(null);
+
+    return (
+        <AppContext.Provider value={{ setIsAuthorized, goals, setGoals }}>
+            {isAuthorized
+                ? (
+                    <Routes>
+                        <Route path={AppRoute.ROOT} element={<Home />} />
+                        <Route path={AppRoute.GOAL_DETAILS} element={<GoalDetailsScreen />} />
+                        <Route path={AppRoute.CREATE_GOAL} element={<CreateGoal />} />
+                        <Route path={AppRoute.FEEDBACK} element={<Feedback />} />
+                        <Route path="/*" element={<Navigate replace to={AppRoute.ROOT} />} />
+                    </Routes>
+                )
+                : (
+                    <Routes>
+                        <Route path={AppRoute.LOGIN} element={<Login />} />
+                        <Route path="/*" element={<Navigate replace to={AppRoute.LOGIN} />} />
+                    </Routes>
+                )
+            }
+        </AppContext.Provider>
+    );
+};
 
 export default App;
---
+++ src/contants.ts
@@ -0,0 +1,8 @@
+export const AppRoute = {
+    ROOT: '/',
+    GOALS: '/goals',
+    GOAL_DETAILS: '/goals/:id',
+    CREATE_GOAL: '/create-goal',
+    FEEDBACK: '/feedback',
+    LOGIN: '/login',
+};
