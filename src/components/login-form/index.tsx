b296070ad542f948b416f5a28de8f9043514d67b
greatstudentkaze <dfyz0r@list.ru>
2022-03-20 10:03:30 +0300

    feat(components/login-form): add pseudo authorization, use proper auto-complete attributes

--- src/components/login-form/index.tsx
+++ src/components/login-form/index.tsx
@@ -1,17 +1,29 @@
+import { FormEvent, useContext } from 'react';
+
+import { AppContext } from '../../app.context';
+
 import InputField from '../input-field';
 import Button from '../button';
 
 const LoginForm = () => {
+    const { setIsAuthorized } = useContext(AppContext);
+
+    const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+        evt.preventDefault();
+        setIsAuthorized(true);
+    };
+
     return (
         <div className="form">
             <h1 className="form__title">Login</h1>
-            <form action="https://echo.htmlacademy.ru" method="post">
+            <form action="https://echo.htmlacademy.ru" method="post" onSubmit={handleSubmit}>
                 <InputField
                     className="form__input"
                     type="email"
                     labelText="Email"
                     id="email"
                     placeholder="hi@nefagin.ru"
+                    autoComplete="username"
                 />
                 <InputField
                     className="form__input"
@@ -19,6 +31,7 @@ const LoginForm = () => {
                     labelText="Password"
                     id="password"
                     placeholder="*************"
+                    autoComplete="current-password"
                 />
                 <Button className="form__submit" type="submit">
                     Login
