a2cdb628f0c9619bb0de4ff3303f7192511264ec
greatstudentkaze <dfyz0r@list.ru>
2022-05-02 01:21:36 +0300

    rename App.tsx to app.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import './styles/index.css';
-import App from './App';
+import App from './app';
 
 ReactDOM.render(
     <React.StrictMode>
