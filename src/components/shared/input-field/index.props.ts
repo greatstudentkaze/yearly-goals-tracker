31ad9cad5d647b51764d9cdb7877b7ab0409e8af
greatstudentkaze <dfyz0r@list.ru>
2022-05-02 13:30:36 +0300

    feat(input-field): add error state, move .css into component directory, add props that come from react-hook-form/register

--- src/components/shared/input-field/index.props.ts
+++ src/components/shared/input-field/index.props.ts
@@ -1,6 +1,10 @@
-import { DetailedHTMLProps, InputHTMLAttributes, ReactNode } from 'react';
+import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import { ChangeHandler, FieldError } from 'react-hook-form';
 
 export interface InputFieldProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
     labelText: string;
     id: string;
+    error?: FieldError;
+    onChange?: ChangeHandler;
+    onBlur?: ChangeHandler;
 }
--- src/components/shared/input-field/index.tsx
+++ src/components/shared/input-field/index.tsx
@@ -1,22 +1,31 @@
+import { forwardRef } from 'react';
 import cn from 'classnames';
 
 import { InputFieldProps } from './index.props';
 
-const InputField = ({
+import './styles/input-field.css';
+
+const InputField = forwardRef<HTMLInputElement, InputFieldProps>(({
     labelText,
     id,
     type = 'text',
     className,
+    error,
     ...props
-}: InputFieldProps) => {
+}, ref) => {
     const isTextArea = type === 'textarea';
 
     return (
-        <div className={cn('input-field', className)}>
+        <div className={cn('input-field', className, {
+            'input-field--error': error,
+        })}>
             <label className="input-field__label" htmlFor={id}>{labelText}</label>
-            <input className="input-field__input" type={type} id={id} {...props} />
+            <input className="input-field__input" type={type} id={id} {...props} ref={ref} />
+            {error?.message && (
+                <p className="input-field__error-message">{error.message}</p>
+            )}
         </div>
     );
-};
+});
 
 export default InputField;
