af5861befcab51938319e14946dbdf674e5f8bda
greatstudentkaze <dfyz0r@list.ru>
2022-06-26 15:29:56 +0300

    feat(components/feedback-form): add feedback submitting

--- src/components/feedback-form/index.tsx
+++ src/components/feedback-form/index.tsx
@@ -1,23 +1,55 @@
-import { SubmitHandler, FieldValues } from 'react-hook-form';
+import { useState } from 'react';
+import { SubmitHandler } from 'react-hook-form';
+
+import Api from '../../api';
+import { useAppSelector } from '../../store/hooks';
+import { selectUser } from '../../store/features/user/slice';
 
 import Form from '../shared/form';
+import SuccessMessage from '../success-message';
 
 import { fieldsConfig } from './fields.config';
 
+interface FormValues {
+    email: string;
+    message: string;
+    name: string;
+}
+
 // TODO: add textArea!
 const FeedbackForm = () => {
-    const handleSubmit: SubmitHandler<FieldValues> = (data, evt) => {
-        console.log(data, evt);
+    const user = useAppSelector(selectUser)!;
+
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [error, setError] = useState('');
+
+    const handleSubmit: SubmitHandler<FormValues> = (data) => {
+        Api.sendFeedback({ ...data, userId: user.id })
+            .then(() => setIsSubmitted(true))
+            .catch((err) => {
+                if (err instanceof Error) {
+                    setError(err.message);
+                }
+            });
     };
+
+    if (isSubmitted) {
+        return (
+            <SuccessMessage/>
+        );
+    }
 
     return (
         <Form
             title="Give feedback / Ask a question"
             inputFieldConfig={fieldsConfig}
             submitText="Send"
-            action="https://echo.htmlacademy.ru"
-            method="post"
             onSubmit={handleSubmit}
+            defaultValues={{
+                name: user.name,
+                email: user.email,
+            }}
+            error={error}
         />
     );
 };
